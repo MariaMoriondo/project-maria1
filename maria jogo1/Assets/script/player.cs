9dcdb167d463c8ceb0892c6faf0e986590a8fc80
estevao9 <131878522+MariaMoriondo@users.noreply.github.com>
2023-07-12 10:16:24 -0300

    inserindo o colaider no player e no tile

--- maria jogo1/Assets/script/player.cs
+++ maria jogo1/Assets/script/player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    //variavel para o rigidBody
+    private RigidBody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<RigidBody2D>();  
     }
 
     // Update is called once per frame
